b435e1dcb03d1f941212584c8c380fccccfec9c2
Gussia835 <cerberweter@gmail.com>
2026-09-03 20:25:45 +0300

    add exceptions

---
+++ src/main/java/org/example/exception/ReceivingFileException.java
@@ -0,0 +1,7 @@
+package org.example.exception;
+
+public class ReceivingFileException extends RuntimeException {
+    public ReceivingFileException(String message) {
+        super(message);
+    }
+}
